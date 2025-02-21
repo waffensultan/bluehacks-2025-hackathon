f1a6cefda4a384ffa6ae68ac8457a2e78d7531e0
Waffen Sultan <waffen.sultan@gmail.com>
2025-02-21 15:10:54 +0800

    feat: updated css and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
             colors: {
                 background: "var(--background)",
                 foreground: "var(--foreground)",
+
+                // Add colors here...
             },
         },
     },
