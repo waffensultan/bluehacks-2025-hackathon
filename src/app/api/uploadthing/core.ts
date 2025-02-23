7368b50321d65fd799c84e0c55da37973b0c0087
Waffen Sultan <waffen.sultan@gmail.com>
2025-02-23 12:34:42 +0800

    fix: eslint update

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,6 +3,7 @@ import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const auth = (req: Request) => ({ id: "fakeId" });
 
 export const ourFileRouter = {
